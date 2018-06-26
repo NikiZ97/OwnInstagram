1a2e8608b02f1f0f6517dd699169976e675b4636
Nikiz1997 <you@example.com>
2018-06-26 15:10:29 +0300

    Custom bottom navigation view added

--- app/src/main/java/com/sharonov/nikiz/nikizinstagram/HomeActivity.kt
+++ app/src/main/java/com/sharonov/nikiz/nikizinstagram/HomeActivity.kt
@@ -2,11 +2,14 @@ package com.sharonov.nikiz.nikizinstagram
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.sharonov.nikiz.nikizinstagram.extensions.setupBottomNavigationView
+import kotlinx.android.synthetic.main.layout_bottom_navigation_view.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        bottomNavigationView.setupBottomNavigationView()
     }
 }
