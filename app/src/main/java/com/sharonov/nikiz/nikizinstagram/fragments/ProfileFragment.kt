698cfabab64b1ba7aa94a39203f1684a3ff53b47
Nikiz1997 <you@example.com>
2018-06-28 16:03:19 +0300

    Profile top snippet added

--- app/src/main/java/com/sharonov/nikiz/nikizinstagram/fragments/ProfileFragment.kt
+++ app/src/main/java/com/sharonov/nikiz/nikizinstagram/fragments/ProfileFragment.kt
@@ -3,11 +3,10 @@ package com.sharonov.nikiz.nikizinstagram.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
+import android.support.v7.app.AppCompatActivity
+import android.view.*
 import com.sharonov.nikiz.nikizinstagram.R
+import kotlinx.android.synthetic.main.snippet_top_profilebar.*
 
 class ProfileFragment : Fragment() {
 
@@ -17,7 +16,29 @@ class ProfileFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        (activity as AppCompatActivity).setSupportActionBar(profileToolbar)
+        profileToolbar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.profile_menu -> {
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.profile_menu, menu)
     }
 }
